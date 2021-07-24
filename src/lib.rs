8595395ab0d26079c989ecd1a724ee9793152c56
NaokiM03 <naokim03.lib+dev@gmail.com>
2021-07-24 16:30:45 +0900

    implement bitand for slices

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,36 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+pub trait SlicesBitAnd<T> {
+    fn bitand_as_usize(&self, other: &Self) -> Option<usize>;
+    fn bitand_as_string(&self, other: &Self) -> Option<String>;
+}
+
+impl<T> SlicesBitAnd<T> for [T] where T: PartialEq {
+    fn bitand_as_usize(&self, other: &Self) -> Option<usize> {
+        if self.len() != other.len() { return None; }
+
+        let mut bit: usize = 0;
+        for i in 0..self.len() {
+            bit <<= 1;
+            if self[i] == other[i] {
+                bit |= 1;
+            } else {
+                bit |= 0;
+            }
+        }
+        Some(bit)
+    }
+
+    fn bitand_as_string(&self, other: &Self) -> Option<String> {
+        if self.len() != other.len() { return None; }
+
+        let mut bit = "".to_string();
+        for i in 0..self.len() {
+            if self[i] == other[i] {
+                bit.push('1');
+            } else {
+                bit.push('0');
+            }
+        }
+
+        Some(bit)
     }
 }
---
+++ tests/test.rs
@@ -0,0 +1,17 @@
+use slices_bitand::SlicesBitAnd;
+
+#[test]
+fn test_usize() {
+    let x = vec![1, 2, 3];
+    let y = vec![1, 2, 4];
+    let z = x.as_slice().bitand_as_usize(y.as_slice());
+    assert_eq!(z, Some(6));
+}
+
+#[test]
+fn test_string() {
+    let x = vec![1, 2, 3];
+    let y = vec![1, 2, 4];
+    let z = x.as_slice().bitand_as_string(y.as_slice());
+    assert_eq!(z, Some("110".to_string()));
+}
