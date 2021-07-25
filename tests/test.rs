57b0bfb03929eff1c5388972384e6a1386b62978
NaokiM03 <naokim03.lib+dev@gmail.com>
2021-07-25 21:11:25 +0900

    add tests for the pattern that returns None

--- tests/test.rs
+++ tests/test.rs
@@ -9,9 +9,25 @@ fn test_usize() {
 }
 
 #[test]
+fn test_usize_different_length() {
+    let x = vec![1, 2];
+    let y = vec![1, 2, 3];
+    let z = x.as_slice().bitand_as_usize(y.as_slice());
+    assert_eq!(z, None);
+}
+
+#[test]
 fn test_string() {
     let x = vec![1, 2, 3];
     let y = vec![1, 2, 4];
     let z = x.as_slice().bitand_as_string(y.as_slice());
     assert_eq!(z, Some("110".to_string()));
+}
+
+#[test]
+fn test_string_different_length() {
+    let x = vec![1, 2];
+    let y = vec![1, 2, 3];
+    let z = x.as_slice().bitand_as_string(y.as_slice());
+    assert_eq!(z, None);
 }
